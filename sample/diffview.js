04ea1e661bc486dae3ee809ad7805be48a701f58
liangdong <liangdong@smzdm.com>
2018-01-30 11:05:37 +0800

    update

--- sample/diffview.js
+++ sample/diffview.js
@@ -1,21 +1,21 @@
 (function ($) {
     var index_head_tpl = '<tr class="index-head"><td colspan="2">{old_file}</td><td colspan="2">{new_file}</td></tr>';
     var seg_head_tpl = '<tr class="seg-head"><td colspan="2">>>>起始行号:{left_start_line}</td><td colspan="2">>>>起始行号:{right_start_line}</td></tr>';
-    var row_tpl = '<tr><td>{left_line}</td><td class="{left_type}">{left_row}</td><td class="{right_type}">{right_line}</td><td>{right_row}</td></tr>';
+    var row_tpl = '<tr><td class="{left_type} line-td">{left_line}</td><td class="{left_type}">{left_row}</td><td class="{right_type} line-td">{right_line}</td><td class="{right_type}">{right_row}</td></tr>';
 
     function generate_index_head(index) {
         var tpl = index_head_tpl
-        return tpl.replace('{old_file}', index.old_file).replace('{new_file}', index.new_file)
+        return tpl.replace(/{old_file}/g, index.old_file).replace(/{new_file}/g, index.new_file)
     }
 
     function generate_seg_head(seg) {
         var tpl = seg_head_tpl
-        return tpl.replace('{left_start_line}', seg.left_start_line).replace('{right_start_line}', seg.right_start_line)
+        return tpl.replace(/{left_start_line}/g, seg.left_start_line).replace(/{right_start_line}/g, seg.right_start_line)
     }
 
     function generate_row(row) {
         var tpl = row_tpl;
-        return tpl.replace('{left_line}', row.left_line).replace('{left_row}', row.left_row).replace('{left_type}', row.left_type).replace('{right_line}', row.right_line).replace('{right_row}', row.right_row).replace('{right_type}', row.right_type)
+        return tpl.replace(/{left_line}/g, row.left_line).replace(/{left_row}/g, $('<div>').text(row.left_row).html()).replace(/{left_type}/g, row.left_type).replace(/{right_line}/g, row.right_line).replace(/{right_row}/g, $('<div>').text(row.right_row).html()).replace(/{right_type}/g, row.right_type)
     }
 
     $.fn.diffview = function(view_data) {
